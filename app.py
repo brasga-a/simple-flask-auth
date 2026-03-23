0d7cf93cfdb0d678f07828bfd35a46f55739eff2
Braga <bradrugo2003@gmail.com>
2026-03-23 19:12:23 -0300

    adding login, register, logout, update_user and delete_user routes to app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, jsonify, request
 from database import db
 from models.user import User
 from auth import login_manager
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user, login_required
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -18,7 +19,9 @@ login_manager.login_view = 'login'
 def health():
     return jsonify({"status": "ok"}), 200
 
+
 @app.route("/")
+@login_required
 def home():
     return '<h1>Welcome to the Flask App!</h1><p>Use the /health endpoint to check the health status.</p>'
 
@@ -42,6 +45,12 @@ def login():
             return jsonify({"message": "Login successful"}), 200
 
     return jsonify({"message": "Invalid credentials"}), 401
+
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout successful"}), 200
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -69,6 +78,46 @@ def register():
         return jsonify({"message": "User registered successfully"}), 201
 
     return jsonify({"message": "Invalid credentials"}), 401
+
+@app.route("/user", methods=["GET"])
+@login_required
+def get_user():
+    user_id = current_user.id
+    user = User.query.get(user_id)
+    if user:
+        return jsonify({"id": user.id, "username": user.username}), 200
+    return jsonify({"message": "User not found"}), 404
+
+@app.route("/user", methods=["PUT"])
+@login_required
+def update_user():
+    user_id = current_user.id
+    data = request.get_json()
+    new_username = data.get("new_username")
+
+    user = User.query.get(user_id)
+    if user:
+        if new_username:
+            user.username = new_username
+            db.session.commit()
+            return jsonify({"message": "User updated successfully"}), 200
+        
+        return jsonify({"message": "No new username provided"}), 400
+
+    return jsonify({"message": "User not found"}), 404
+
+@app.route("/user", methods=["DELETE"])
+@login_required
+def deleteu_user():
+    user_id = current_user.id
+    user = User.query.get(user_id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        logout_user()
+        return jsonify({"message": "User deleted successfully"}), 200
+    
+    return jsonify({"message": "User not found"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
